581c988b685f320f1e26a63a89f117010a12c857
Kartik Jevaji <9121718+nitrotap@users.noreply.github.com>
2023-06-22 23:44:58 -0600

    adding jwt logic to login and signup api

---
+++ server/api/user/auth/login.php
@@ -0,0 +1,105 @@
+<?php
+// Check if request method is OPTIONS
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    // Respond to preflight request
+    header('Access-Control-Allow-Origin: *'); // Allow requests from any origin
+    header('Access-Control-Allow-Methods: POST, GET, OPTIONS'); // Allow these methods
+    header('Access-Control-Allow-Headers: Content-Type'); // Allow this header
+    header('Content-Type: application/json');
+    exit(0); // No further processing if OPTIONS request
+}
+
+require(__DIR__ . '../../../../vendor/autoload.php');
+
+use Firebase\JWT\JWT;
+
+include(__DIR__ . '../../../env.php');
+include(__DIR__ . '../../../sanitize.php');
+
+// Specify table
+$table = 'user_table';
+
+// Establish a connection to the database
+try {
+    $db = new PDO("mysql:host=$host;dbname=$dbName", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+
+// API endpoint for inserting data into a table
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    sanitizeRequestStrings();
+
+
+    // Retrieve data from the request body
+    $requestData = $_REQUEST;
+
+
+    // get user email
+    $query = "SELECT * FROM $table WHERE email = :email";
+    $stmt = $db->prepare($query);
+    $stmt->bindParam(':email', $requestData['email']);
+    $stmt->execute();
+
+
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if (!$user) {
+        header('Content-Type: application/json');
+        header('Access-Control-Allow-Origin: *'); // Allow requests from any origin
+        http_response_code(401);
+        echo json_encode(['error' => 'No user found for this email.']);
+        exit();
+    }
+
+    // Verify password
+    if (!password_verify($_REQUEST['password'], $user['password'])) {
+        header('Content-Type: application/json');
+        header('Access-Control-Allow-Origin: *'); // Allow requests from any origin
+
+        http_response_code(401);
+        echo json_encode(['error' => 'Password is incorrect.']);
+        exit();
+    }
+
+    session_start();
+
+    // save session id to mysql database in user_table for column session_id
+    $sessionId = session_id();
+
+    $payload = [
+        "userID" => $user['userID'],
+        "email" => $user['email'],
+        "iat" => time(),
+        "exp" => time() + (60 * 60), // 1 hour expiration time,
+        "access" => true,
+        "sessionID" => $sessionId
+    ];
+    $jwt = JWT::encode($payload, 'Zgl68pwKQFf9N88A', 'HS256');
+
+
+    $updateQuery = "UPDATE $table SET session_id=:session_id WHERE email=:email";
+    $updateStmt = $db->prepare($updateQuery);
+//  $updateStmt->bindParam(':session_id', $sessionId);
+    $updateStmt->bindParam(':session_id', $jwt);
+
+    $updateStmt->bindParam(':email', $requestData['email']);
+    $updateStmt->execute();
+
+
+    // Return success response
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Origin: *'); // Allow requests from any origin
+//    header('Authorization: Bearer ' . session_id());
+
+    echo json_encode(array(
+        'message' => 'Login successful!',
+        'sessionID' => session_id(),
+        'Authorization' => 'true',
+        'userID' => $user['userID'],
+        'jwt' => $jwt
+    ));
+}
---
+++ server/api/user/auth/signup.php
@@ -0,0 +1,121 @@
+<?php
+// Check if request method is OPTIONS
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    // Respond to preflight request
+    header('Access-Control-Allow-Origin: *'); // Allow requests from any origin
+    header('Access-Control-Allow-Methods: POST, GET, OPTIONS'); // Allow these methods
+    header('Access-Control-Allow-Headers: Content-Type'); // Allow this header
+    header('Content-Type: application/json');
+    exit(0); // No further processing if OPTIONS request
+}
+require(__DIR__ . '../../../../vendor/autoload.php');
+
+use Firebase\JWT\JWT;
+
+
+include(__DIR__ . '../../../env.php');
+include(__DIR__ . '../../../sanitize.php');
+
+// Specify table
+$table = 'user_table';
+
+// Establish a connection to the database
+try {
+    $db = new PDO("mysql:host=$host;dbname=$dbName", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+
+// API endpoint for retrieving data from a table
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // Retrieve data from the table
+    $query = "SELECT * FROM $table";
+    $stmt = $db->prepare($query);
+    $stmt->execute();
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $sessionId = session_start();
+
+
+    try {
+        $payload = [
+            "userID" => $data['userID'],
+            "email" => $data['email'],
+            "iat" => time(),
+            "exp" => time() + (60 * 60), // 1 hour expiration time,
+            "access" => true,
+            "sessionID" => $sessionId
+        ];
+        $jwt = JWT::encode($payload, 'Zgl68pwKQFf9N88A', 'HS256');
+        header('Content-Type: application/json');
+        header('Access-Control-Allow-Origin: *'); // Allow requests from any origin
+        echo json_encode($jwt);
+
+
+    } catch (error $e) {
+        header('Content-Type: application/json');
+        header('Access-Control-Allow-Origin: *'); // Allow requests from any origin
+        echo json_encode($e);
+    }
+
+
+    // Return the data as JSON response
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Origin: *'); // Allow requests from any origin
+    echo json_encode($data);
+
+}
+
+// API endpoint for inserting data into a table
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    sanitizeRequestStrings();
+    $hash = password_hash($_REQUEST["password"], PASSWORD_BCRYPT);
+
+    $_REQUEST["password"] = $hash;
+
+    $requestData = $_REQUEST;
+
+    // Insert the data into the table
+    $query = "INSERT INTO $table (email, password) VALUES (:value1, :value2)";
+    $stmt = $db->prepare($query);
+    $stmt->bindParam(':value1', $requestData['email']);
+    $stmt->bindParam(':value2', $requestData['password']);
+
+    $stmt->execute();
+
+    $data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+
+    // Return success response
+    session_start();
+    $sessionId = session_id();
+    $payload = [
+        "userID" => $data['userID'],
+        "email" => $data['email'],
+        "iat" => time(),
+        "exp" => time() + (60 * 60), // 1 hour expiration time,
+        "access" => true,
+        "sessionID" => $sessionId
+    ];
+    $jwt = JWT::encode($payload, 'Zgl68pwKQFf9N88A', 'HS256');
+
+    // save session id to mysql database in user_table for column session_id
+    $updateQuery = "UPDATE $table SET session_id=:session_id WHERE email=:email";
+    $updateStmt = $db->prepare($updateQuery);
+//  $updateStmt->bindParam(':session_id', $sessionId);
+    $updateStmt->bindParam(':session_id', $jwt);
+
+    $updateStmt->bindParam(':email', $requestData['email']);
+    $updateStmt->execute();
+
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Origin: *'); // Allow requests from any origin
+    echo json_encode(array(
+        'message' => 'Signup Successful!',
+        'sessionID' => $sessionId,
+        'Authorization' => 'true',
+        'jwt' => $jwt
+    ));
+}
